95c771733686a596eebe798e4546fb3679d0df3e
GreywindGit <GreywindGit@users.noreply.github.com>
2018-01-15 13:41:45 +0100

    Pass arguments to event handler

--- src/App.js
+++ src/App.js
@@ -11,32 +11,38 @@ class App extends Component {
       ]
   }
 
-  switchWolfHandler = () => {
+  switchWolfHandler = (newAge) => {
       this.setState( {
         wolves: [
-          { name: "Cassira", age: "6" },
+          { name: "Cassira", age: newAge },
           { name: "Aldway", age: "12"},
           { name: "Rosie", age: "2"}
         ]
       });
   }
 
+  /*
+    Two ways of passing arguments of methods: this.functionName.bind(this, newValue) OR
+    () => this.functionName(newValue). By binding it, the second "this" keyword refers 
+    to the object that the function refers to!
+  */
   render() {
     return (
       <div className="App">
           <h1>Hello World!</h1>
           <p>This is my first React app running on my laptop.</p>
-          <button onClick={this.switchWolfHandler}>Switch Wolf</button>
+          <button onClick={() => this.switchWolfHandler("17")}>Switch Wolf</button>
           <Person 
             name={this.state.wolves[0].name} 
             age={this.state.wolves[0].age}>I love full moons!</Person>
           <Person 
             name={this.state.wolves[1].name} 
             age={this.state.wolves[1].age}
-            click={this.switchWolfHandler}>I'm quite happy and peaceful.</Person>
+            click={this.switchWolfHandler.bind(this, "15")}>I'm quite happy and peaceful.</Person>
           <Person 
             name={this.state.wolves[2].name} 
-            age={this.state.wolves[2].age}>I'm just a puppy yet :)</Person>
+            age={this.state.wolves[2].age}
+            click={this.switchWolfHandler.bind(this, "19")}>I'm just a puppy yet :)</Person>
       </div>
     );
   }
